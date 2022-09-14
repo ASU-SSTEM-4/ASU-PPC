ab70b83610935c7a03cfeaf3af8afb7f51a01cf8
Curt Bridgers <prestonbridgers@gmail.com>
2022-09-14 17:58:33 -0400

    Added connectivity of the backend to the mongodb backend.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb+srv://team4:sstem@asu-ppc-data.gg5ivqy.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
+    // Connecting to mongo database
+    res = MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+
+        const datab = db.db("PPC");
+        datab.collection("user-data").findOne({}, function(err, res) {
+            if (err) throw err;
+            console.log(res.message);
+            db.close();
+        });
+    });
 });
 
 app.listen(PORT, () => {
