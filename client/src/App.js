b9cd16b246d34a4b6a2e8c6cbb47bc6b7455d33d
lewisaj1 <lewisaj1@appstate.edu>
2022-09-14 17:56:45 -0400

    Made Tailwind Nav and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Login from "./Pages/Public/Login/Login";
+import Form from "./Pages/Private/Form/Form";
+import Preview from "./Pages/Private/Preview/Preview";
+import Home from "./Pages/Public/Home/Home";
 import './App.css';
-import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Hero/>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/Login' element={<Login/>}></Route>
+      <Route path='/Form' element={<Form/>}></Route>
+      <Route path='/Preview' element={<Preview/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
