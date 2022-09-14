5ccb0e826f47afce99ab64de76c817cdaabd0a56
Curt Bridgers <prestonbridgers@gmail.com>
2022-09-14 17:22:32 -0400

    Added server backend with a single /api endpoint that sends a string to the client to be displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Hero from './components/Hero/Hero';
+import React from 'react';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Hero/>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
