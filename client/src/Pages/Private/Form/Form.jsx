5a28a5801603a057f3949460426057e62e390ff7
lewisaj1 <lewisaj1@appstate.edu>
2022-10-05 17:40:48 -0400

    Template for body

--- client/src/Pages/Private/Form/Form.jsx
+++ client/src/Pages/Private/Form/Form.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import TeacherForm from '../../../Components/TeacherForm/TeacherForm'
 
 export default class Form extends Component {
   render() {
     return (
-      <div>This is = Form</div>
+      <div><TeacherForm/></div>
     )
   }
 }
